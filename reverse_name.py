ac4ebfc86f8cb8bd171b5425ebaada72ac0dc237
Uthaeus <romanlavery@gmail.com>
2018-04-23 20:11:03 -0600

    Reverse name

---
+++ reverse_name.py
@@ -0,0 +1,9 @@
+# Write a Python program which accepts the user's first and last name and print them in reverse order with a space between them.
+
+print("Welcome!")
+first = input("Enter your first name:\n")
+last = input("Enter your last name:\n")
+
+result = f"Welcome {last}, {first}"
+
+print(result)
