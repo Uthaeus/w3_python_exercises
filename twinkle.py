adb414566696c5412b750e87ec8ae1eed4d30a4c
Uthaeus <romanlavery@gmail.com>
2018-04-23 19:39:34 -0600

    Twinkle twinkle

---
+++ twinkle.py
@@ -0,0 +1,19 @@
+l_one = 'Twinkle, twinkle, little star,'
+l_two = 'How I wonder what you are!'
+l_three = 'Up above the world so high,'
+l_four = 'Like a diamond in the sky.'
+
+# space = ' '
+
+# print(l_one)
+# print((space * 25) + l_two)
+# print((space * 50) + l_three)
+# print((space * 50) + l_four)
+# print((space * 25) + l_two)
+# print(l_one)
+
+content = """
+Twinkle, Twinkle, little star,\n\tHow I wonder what you are!\n\t\tUp above the world so high,\n\t\tLike a diamond in the sky.\nTwinkle, twinkle, little star,\n\tHow I wonder what you are.
+"""
+
+print(content)
