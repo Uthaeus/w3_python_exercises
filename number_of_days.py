1023364b5b7b96503b958c63c51a3ce3fa0748f7
Uthaeus <romanlavery@gmail.com>
2018-04-24 10:24:09 -0600

    Calculate the number of days between two dates

---
+++ number_of_days.py
@@ -0,0 +1,11 @@
+# Write a Python program to calculate number of days between two dates.
+# Sample dates : (2014, 7, 2), (2014, 7, 11)
+
+from datetime import date 
+
+first = date(2014, 7, 2)
+last = date(2014, 7, 11)
+
+result = last - first
+
+print(result.days)
